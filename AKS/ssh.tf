1be6d69c7c4dc2829a7a3f1ed4de0b0d0833d040
mostechsystem <mostechsystem@gmail.com>
2025-01-03 18:22:15 +0100

    Update ssh.tf

--- AKS/ssh.tf
+++ AKS/ssh.tf
@@ -4,7 +4,7 @@ resource "random_pet" "ssh_key_name" {
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type        = "Microsoft.Compute/sshPublicKeys@2024-07-01"
   resource_id = azapi_resource.ssh_public_key.id
   action      = "generateKeyPair"
   method      = "POST"
@@ -13,7 +13,7 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type      = "Microsoft.Compute/sshPublicKeys@2024-07-01"
   name      = random_pet.ssh_key_name.id
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
