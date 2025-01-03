3ace4185bfa5fb1d59536fe36bd2a24a1c1aa2ed
mostechsystem <mostechsystem@gmail.com>
2025-01-03 18:08:12 +0100

    Update ssh.tf

--- AKS/ssh.tf
+++ AKS/ssh.tf
@@ -3,16 +3,6 @@ resource "random_pet" "ssh_key_name" {
   separator = ""
 }
 
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
-  parent_id = azurerm_resource_group.rg.id
-}
-  # Ensure that the random_pet resource is created before this
-  depends_on = [random_pet.ssh_key_name]
-}
-
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
@@ -20,9 +10,13 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
   method      = "POST"
 
   response_export_values = ["publicKey", "privateKey"]
+}
 
-  # Ensure that the azapi_resource is created before this action
-  depends_on = [azapi_resource.ssh_public_key]
+resource "azapi_resource" "ssh_public_key" {
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = random_pet.ssh_key_name.id
+  location  = azurerm_resource_group.rg.location
+  parent_id = azurerm_resource_group.rg.id
 }
 
 output "key_data" {
